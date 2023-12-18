486c162c7775672ddb200793fb3d3018fd56b307
d067388 <juri.zeisler@sap.com>
2023-12-18 21:32:05 +0100

    day18

---
+++ src/main/java/day18/CoordinateChecker.java
@@ -0,0 +1,12 @@
+package day18;
+
+import day3.Coordinate;
+
+public class CoordinateChecker {
+    public static boolean isValid(char[][] map, Coordinate coordinate) {
+        if (coordinate.y() < 0 || coordinate.x() < 0) {
+            return false;
+        }
+        return coordinate.y() < map.length && coordinate.x() < map[0].length;
+    }
+}
--- src/main/java/day3/Coordinate.java
+++ src/main/java/day3/Coordinate.java
@@ -1,5 +1,13 @@
 package day3;
 
-public record Coordinate(int x, int y) {
+public record Coordinate(int x, int y) implements Comparable<Coordinate> {
+
+    @Override
+    public int compareTo(Coordinate o) {
+        if (this.y() == o.y()) {
+            return Integer.compare(this.x(), o.x());
+        }
+        return Integer.compare(this.y(), o.y());
+    }
 
 }
